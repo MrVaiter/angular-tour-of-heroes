4d6446cd19010068b4e235b464fe53151e59f425
Dmytro Molchanov <dima-25@ukr.net>
2022-12-14 17:19:53 +0200

    Add a hero property

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
-  constructor(){}
-  
-  ngOnInit(): void {}
+
+  hero = 'Windstorm';
+
+  constructor() { }
+
+  ngOnInit(): void { }
 
 }
