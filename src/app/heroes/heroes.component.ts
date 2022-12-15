02342f1421de20bae3013d64f624b59adb561152
Dmytro Molchanov <dima-25@ukr.net>
2022-12-15 15:29:43 +0200

    Create navigation to hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,10 +12,8 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService,
-    private messageService: MessageService) {
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit(): void {
@@ -25,11 +23,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
                     .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
