159f5fd10a03b38466543e88457099bc1a4bd35d
Dmytro Molchanov <dima-25@ukr.net>
2022-12-16 17:11:05 +0200

    Make pipe async

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
 
-import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
