89967c0c2e84a981cd8f71d40480caa059cec1d1
Dmytro Molchanov <dima-25@ukr.net>
2022-12-22 14:00:45 +0200

    Add GetTeams() to TeamService

--- src/app/components/teams-list/teams-list.component.ts
+++ src/app/components/teams-list/teams-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Account } from 'src/app/interfaces/account';
+
+import { Team } from 'src/app/interfaces/team';
+import { AccountService } from 'src/app/services/account.service';
+import { TeamService } from 'src/app/services/team.service';
 
 @Component({
   selector: 'app-teams-list',
   templateUrl: './teams-list.component.html',
   styleUrls: ['./teams-list.component.css']
 })
-export class TeamsListComponent {
+export class TeamsListComponent implements OnInit {
+
+  teams: Team[] = [];
+  players: Account[] = []
+
+  constructor(
+    private teamService: TeamService,
+    private playerService: AccountService) { }
+
+  ngOnInit(): void {
+    this.teamService.getTeams().subscribe(teams => {
+      this.teams = teams;
+      console.log(this.teams);
+    });
+
+    this.playerService.getAccounts().subscribe(accounts => {
+      this.players = accounts.filter(account => account.role == 'user');
+      console.log(this.players);
+    });
+  }
 
 }
--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+import { MessageService } from './message.service';
+
+import { catchError, Observable, of, tap } from 'rxjs';
+
+import { Team } from '../interfaces/team';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor() { }
+  private teamUrl = 'api/teams';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) { }
+
+  getTeams(): Observable<Team[]>{
+    return this.http.get<Team[]>(this.teamUrl)
+    .pipe(
+      tap(_ => this.log('fetched teams')),
+      catchError(this.handleError<Team[]>(`getTeams`))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`AccountService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
+  }
 }
