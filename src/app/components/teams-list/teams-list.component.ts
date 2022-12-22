d3012d71951e6b8646399d304e0760b4c53ea9c9
Dmytro Molchanov <dima-25@ukr.net>
2022-12-22 17:22:58 +0200

    Create addTeam() in TeamsListComponent

--- src/app/components/teams-list/teams-list.component.ts
+++ src/app/components/teams-list/teams-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Account } from 'src/app/interfaces/account';
 
 import { Team } from 'src/app/interfaces/team';
@@ -14,11 +15,17 @@ export class TeamsListComponent implements OnInit {
 
   isAdmin = document.cookie == 'admin';
   teams: Team[] = [];
-  players: Account[] = []
+  players: Account[] = [];
+  myForm: FormGroup;
 
   constructor(
     private teamService: TeamService,
-    private playerService: AccountService) { }
+    private playerService: AccountService) {
+
+    this.myForm = new FormGroup({
+      "teamName": new FormControl("", [Validators.required, Validators.minLength(4)])
+    });
+  }
 
   ngOnInit(): void {
     this.teamService.getTeams().subscribe(teams => {
@@ -30,6 +37,17 @@ export class TeamsListComponent implements OnInit {
     });
   }
 
+  addTeam() {
 
+    let newTeam: Team = {
+      id: this.teams.length + 1,
+      name: this.myForm.get('teamName')?.value,
+      members: []
+    };
+
+    this.teamService.addTeam(newTeam).subscribe(_ => {
+      this.teams.push(newTeam)
+    });
+  }
 
 }
